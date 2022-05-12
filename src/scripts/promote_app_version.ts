f6b65b409f6bed19260e65f976734ae8c7563038
Lorenzo Greco <lorenzo.rev@gmail.com>
2022-05-12 16:57:12 +0200

    remove unused code

--- src/scripts/promote_app_version.ts
+++ src/scripts/promote_app_version.ts
@@ -2,16 +2,6 @@ import { simpleLogger } from 'adash-ts-helper';
 import 'colors';
 import simpleGit, { ResetMode, SimpleGit } from 'simple-git';
 
-export function extractVersions(data: unknown) {
-  const versionRegExp = /\/(?<version>\d+\.\d{2}\.\d)/g; // matches 5.25.0 from feat/5.25.0/SYST-000-title
-  const strData = JSON.stringify(data);
-  const versions = strData
-    .match(versionRegExp)
-    .map((v) => v.replace(/\//g, ''))
-    .flat();
-  return versions[0];
-}
-
 const git: SimpleGit = simpleGit({});
 const logger = simpleLogger();
 
